3b36ff6ec796dd86b1be5e1f9ba832b79cd8ec6c
huoxinghua <huoxinghuacn@gmail.com>
2025-01-17 20:54:56 -0700

    just one player now; still need change something. add indicator and simplay highlight

---
+++ Assets/Xinghua/Scripts/Grid/GridIndicator.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using static UnityEditorInternal.VersionControl.ListControl;
+
+public class GridIndicator : MonoBehaviour
+{
+    [SerializeField] private GameObject heroPrefab;
+    [SerializeField] private Vector2 gridSize = new Vector2(-100, 100); 
+    [SerializeField] private float tileSize = 1f; 
+    private Vector2 currentGridPosition; 
+
+    void Start()
+    {
+        //should get the current player position
+        transform.position = heroPrefab.transform.position;
+    }
+
+    void Update()
+    {
+        HandleInput();
+        UpdateIndicatorPosition();
+    }
+
+    private void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.W)) MoveIndicator(Vector2.up);
+        if (Input.GetKeyDown(KeyCode.S)) MoveIndicator(Vector2.down);
+        if (Input.GetKeyDown(KeyCode.A)) MoveIndicator(Vector2.left);
+        if (Input.GetKeyDown(KeyCode.D)) MoveIndicator(Vector2.right);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 targetPosition = transform.position;
+            heroPrefab.transform.position = targetPosition;
+        }
+    }
+
+    private void MoveIndicator(Vector2 direction)
+    { 
+        Vector2 newPosition = currentGridPosition + direction;  
+        if (newPosition.x >= 0 && newPosition.x < gridSize.x &&
+            newPosition.y >= 0 && newPosition.y < gridSize.y)
+        {
+            currentGridPosition = newPosition; 
+        }
+    }
+
+    private void UpdateIndicatorPosition()
+    {
+        Vector3 worldPosition = new Vector3(currentGridPosition.x * tileSize, currentGridPosition.y * tileSize, 0);
+        transform.position = worldPosition;
+    }
+}
---
+++ Assets/Xinghua/Scripts/Player/Highlight.cs
@@ -0,0 +1,61 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class HighLight : MonoBehaviour
+{
+    [SerializeField] private GameObject highlightPrefab;
+    [SerializeField] private LayerMask gridLayer;
+    [SerializeField] private float tileSize = 1f;
+
+    private GameObject[] highlights = new GameObject[4];
+    private Vector3[] directions;
+    private int selectedDirection = 0;
+
+    void Start()
+    {
+        // initialize 
+        directions = new Vector3[]
+        {
+            Vector3.up * tileSize,
+            Vector3.down * tileSize,
+            Vector3.left * tileSize,
+            Vector3.right * tileSize
+        };
+
+         for (int i = 0; i < 4; i++)
+        {
+            highlights[i] = Instantiate(highlightPrefab);
+            highlights[i].SetActive(false);
+        }
+    }
+
+    private void UpdateHighlights()
+    {
+        Vector3 playerPosition = transform.position;
+        for (int i = 0; i < highlights.Length; i++)
+        {
+            Vector3 targetPosition = playerPosition + directions[i];
+            highlights[i].transform.position = targetPosition;
+            highlights[i].SetActive(CanMoveTo(targetPosition));
+        }
+
+        for (int i = 0; i < highlights.Length; i++)
+        {
+            highlights[i].GetComponent<SpriteRenderer>().color = (i == selectedDirection) ? Color.yellow : Color.white;
+        }
+    }
+
+    private bool CanMoveTo(Vector3 targetPosition)
+    {
+        Collider2D hit = Physics2D.OverlapCircle(targetPosition, 0.1f, gridLayer);
+        return hit == null;
+    }
+    private void HideIndicators()
+    {
+        foreach (var highlight in highlights)
+        {
+            highlight.SetActive(false);
+        }
+
+    }
+}
