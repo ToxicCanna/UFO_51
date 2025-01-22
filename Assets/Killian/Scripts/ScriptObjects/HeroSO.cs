973bdddf639e70e2b06a50427d4fa843786ed82c
Toxic <freedom22140@gmail.com>
2025-01-21 19:32:14 -0700

    Prefab Update
    -Added heros for each side, organized folders, created remaining hero objects, added cost to units
    -prototype death and upgrade, health update

--- Assets/Killian/Scripts/ScriptObjects/HeroSO.cs
+++ Assets/Killian/Scripts/ScriptObjects/HeroSO.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Hero", menuName = "ScriptableObjects/HeroSO", order = 1)]
 public class HeroSO : ScriptableObject
 {
-    public int health, atk, atkSize, def, defSize, heal, healSize, moveSpeed, range, ability;
+    public int health, cost, atk, atkSize, def, defSize, heal, healSize, moveSpeed, range, ability;
     public string abilityScore;
     public HeroSO Upgrade;
 
