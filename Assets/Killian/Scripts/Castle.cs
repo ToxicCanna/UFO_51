4d94371dfa54d676433d57a196f68071b679fa3b
Toxic <freedom22140@gmail.com>
2025-01-27 21:20:21 -0700

    Castle HP bar update
    castle HP implemented, gradient implemented

--- Assets/Killian/Scripts/Castle.cs
+++ Assets/Killian/Scripts/Castle.cs
@@ -1,23 +1,36 @@
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Castle : MonoBehaviour
 {
     public int maxHP;
     public int castleHP;
 
-    private void Start()
+    [SerializeField] private Image _image;
+    [SerializeField] private Gradient _gradient;
+    private float _target;
+
+    private void Awake()
     {
         castleHP = maxHP;
+        CheckHealthBarGradientAmount();
     }
 
     public void TakeDamage(int damage)
     {
         castleHP -= damage;
+        _target = castleHP/maxHP;
+        _image.fillAmount = _target;
         if (castleHP <= 0)
         {
             LoseGame();
         }
+        CheckHealthBarGradientAmount();
+    }
+    private void CheckHealthBarGradientAmount()
+    {
+        _image.color = _gradient.Evaluate(_image.fillAmount);
     }
     public virtual void LoseGame()
     {
