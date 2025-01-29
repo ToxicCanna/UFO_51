f69755c3635cd342c311265754a87088bc56f767
huoxinghua <huoxinghuacn@gmail.com>
2025-01-29 09:46:07 -0700

    create the attack state

---
+++ Assets/Xinghua/Scripts/Utilities/GamePlayState/AttackState.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class AttackState : BaseState
+{
+    public override void EnterState()
+    {
+        
+    }
+
+    public override void UpdateState()
+    {
+        
+    }
+
+    public override void ExitState()
+    {
+        
+    }
+    
+    public override void HandleInput(InputManager inputManager) { }
+}
