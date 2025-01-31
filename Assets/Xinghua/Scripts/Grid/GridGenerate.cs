c770050d3b89312936f9ea190307baed346d45d8
huoxinghua <68455980+huoxinghua@users.noreply.github.com>
2025-01-30 21:43:46 -0700

    combine with Castle

--- Assets/Xinghua/Scripts/Grid/GridGenerate.cs
+++ Assets/Xinghua/Scripts/Grid/GridGenerate.cs
@@ -12,7 +12,7 @@ public class GrideGenerate : MonoBehaviour
     private void Start()
     {
         GenerateGrid();
-        //SetCameraPosition();
+        SetCameraPosition();
     }
     private void GenerateGrid()
     {
