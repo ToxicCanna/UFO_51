c84617a603003a5fa186b8e87f50cca491508655
huoxinghua <68455980+huoxinghua@users.noreply.github.com>
2025-02-02 14:28:41 -0700

    create game Manager to manage coin and hero state etc. gmae data

--- Assets/Xinghua/Scripts/Utilities/GameManager.cs
+++ Assets/Xinghua/Scripts/Utilities/GameManager.cs
@@ -1,10 +1,15 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using static UnityEngine.Rendering.DebugUI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
     public bool IsHeroSubmitted { get; private set; }
+    public int coinCount;
+    public TMP_Text coinText;
 
     private void Awake()
     {
@@ -15,9 +20,22 @@ public class GameManager : MonoBehaviour
         }
         Instance = this;
     }
+    private void Start()
+    {
+        // coinCount =3; 
+        UpdateCoinCount(3);
+    }
     public void UpdateHeroSubmissionState(bool state)
     {
         IsHeroSubmitted = state;
     }
+    public void UpdateCoinCount(int value)
+    {
+        coinCount += value;
+        coinText.text = coinCount.ToString();   
+    }
 
 }
+
+
+
