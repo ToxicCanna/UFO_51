03044281a0303a957619a2a9d2d1e6f7f7f9d3a2
Toxic <freedom22140@gmail.com>
2025-02-12 11:48:38 -0700

    Visual Dice Roll Prefab

--- Assets/Killian/Scripts/VisualRollGen.cs
+++ Assets/Killian/Scripts/VisualRollGen.cs
@@ -1,20 +1,47 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class VisualRollGen : MonoBehaviour
 {
     [SerializeField] private Sprite[] sides;
+    public SpriteRenderer spriteRen;
 
     private Sprite sprite;
-    private int side;
+    private int randomSide;
+    private IEnumerator coroutine;
     public void ShowRoll(int result)
     {
-        if (result <= sides.Length)
+        for (int i = 0; i <= 6; i++)
         {
-            side = UnityEngine.Random.Range(0, sides.Length);
-
-            sprite = sides[side];
+            if (i < 6)
+            {
+                coroutine = RollDelay((i + 1) * .25f);
+                StartCoroutine(coroutine);
+                Debug.Log($"{i} i is");
+            }
+            else if (i == 6)
+            {
+                coroutine = RollDelaySetsResult((i + 1) * 0.25f, result);
+                StartCoroutine(coroutine);
+            }
         }
+    }
 
+    private IEnumerator RollDelay(float tick)
+    {
+        yield return new WaitForSeconds(tick);
+                randomSide = UnityEngine.Random.Range(0, sides.Length);
+        sprite = sides[randomSide];
+        spriteRen.sprite = sprite;
+        Debug.Log($"{randomSide + 1} rolled visual");
+    }
+
+    private IEnumerator RollDelaySetsResult(float tick, int result)
+    {
+        yield return new WaitForSeconds(tick);
+        sprite = sides[result-1];
+        spriteRen.sprite = sprite;
+        Debug.Log($"{result} rolled visual");
     }
 }
